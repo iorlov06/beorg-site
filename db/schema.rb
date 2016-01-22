0d3d3c7e50e479bbeb14ac9d20bae066adbf602d
Mike Yurchenkov <m.yurchenkov@mt.ru>
2016-01-22 11:20:24 +0300

    added status to order class

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121150823) do
+ActiveRecord::Schema.define(version: 20160122081925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160121150823) do
     t.integer  "user_id"
     t.integer  "departure_point_id"
     t.integer  "destination_point_id"
+    t.integer  "status"
   end
 
   add_index "orders", ["parcel_id"], name: "index_orders_on_parcel_id", using: :btree
