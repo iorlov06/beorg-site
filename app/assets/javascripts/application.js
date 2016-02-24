36b18f216bc18ef96210c3c65f340e67fcb27817
riozaki <orlov06@gmail.com>
2016-02-25 01:28:46 +0300

    News edition, creation and layout changes. Minor fixes on main page News showing/adition/creation is transfered into pop-up window

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,19 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function Popup(link){
+    window.open(link.href,'details','toolbar=no, location=no, resizable=no,' +
+        ' width=800px, height=400px');
+    return false;
+}
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#F7Fc88', '#94Fc88', '#88E1FC', 'whitesmoke', 'black'],
+        anchors: ['home', 'about', 'news', 'reviews'],
+        menu: '#menu',
+        scrollingSpeed: 800
+    });
+
+});
